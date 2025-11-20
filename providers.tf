4bae1205da424996ff3f83031b9cfffb8a75809b
mmichel122 <mikael.michelier@googlemail.com>
2025-11-20 15:31:47 +0100

    backend added

--- providers.tf
+++ providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "eks-automation-jenkins-state"
+    key    = "pipeline/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
