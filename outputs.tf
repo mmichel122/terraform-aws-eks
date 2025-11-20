c92bf2d8be2640b48402f8a3ab61cb1769f82e53
mmichel122 <mikael.michelier@googlemail.com>
2025-11-20 16:08:48 +0100

    disable eks cluster v2

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "cluster_name" {
-  description = "Name of the EKS cluster."
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "Name of the EKS cluster."
+#   value       = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for the EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for the EKS control plane."
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "cluster_ca_certificate" {
-  description = "Base64 encoded certificate data required to communicate with the cluster."
-  value       = module.eks.cluster_ca_certificate
-}
+# output "cluster_ca_certificate" {
+#   description = "Base64 encoded certificate data required to communicate with the cluster."
+#   value       = module.eks.cluster_ca_certificate
+# }
 
-output "node_group_name" {
-  description = "Name of the managed node group."
-  value       = module.node_group.node_group_name
-}
+# output "node_group_name" {
+#   description = "Name of the managed node group."
+#   value       = module.node_group.node_group_name
+# }
 
 output "vpc_id" {
   description = "ID of the created VPC."
