7294b63b7713aa643310d083062dbdfb245cddde
mmichel122 <mikael.michelier@googlemail.com>
2025-11-20 16:07:08 +0100

    disable eks cluster

--- main.tf
+++ main.tf
@@ -15,28 +15,28 @@ module "iam" {
   tags         = var.tags
 }
 
-module "eks" {
-  source = "./modules/eks"
+# module "eks" {
+#   source = "./modules/eks"
 
-  cluster_name       = var.cluster_name
-  subnet_ids         = concat(module.vpc.public_subnet_ids, module.vpc.private_subnet_ids)
-  vpc_id             = module.vpc.vpc_id
-  vpc_cidr           = var.vpc_cidr
-  cluster_role_arn   = module.iam.cluster_role_arn
-  kubernetes_version = var.eks_version
-  allow_external_ips = var.allow_external_ips
-  tags               = var.tags
-}
+#   cluster_name       = var.cluster_name
+#   subnet_ids         = concat(module.vpc.public_subnet_ids, module.vpc.private_subnet_ids)
+#   vpc_id             = module.vpc.vpc_id
+#   vpc_cidr           = var.vpc_cidr
+#   cluster_role_arn   = module.iam.cluster_role_arn
+#   kubernetes_version = var.eks_version
+#   allow_external_ips = var.allow_external_ips
+#   tags               = var.tags
+# }
 
-module "node_group" {
-  source = "./modules/node_group"
+# module "node_group" {
+#   source = "./modules/node_group"
 
-  cluster_name   = module.eks.cluster_name
-  subnet_ids     = module.vpc.private_subnet_ids
-  node_role_arn  = module.iam.node_role_arn
-  desired_size   = var.node_desired_size
-  min_size       = var.node_min_size
-  max_size       = var.node_max_size
-  instance_types = [var.node_instance_type]
-  tags           = var.tags
-}
+#   cluster_name   = module.eks.cluster_name
+#   subnet_ids     = module.vpc.private_subnet_ids
+#   node_role_arn  = module.iam.node_role_arn
+#   desired_size   = var.node_desired_size
+#   min_size       = var.node_min_size
+#   max_size       = var.node_max_size
+#   instance_types = [var.node_instance_type]
+#   tags           = var.tags
+# }
